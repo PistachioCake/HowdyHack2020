a8e747fa7d23bc71ed1e8ad591055e9e1195c5de
Rushil Udani <rushil.udani@gmail.com>
2020-09-05 18:05:49 -0500

    Use folium to create a heatmap!
    Create a heatmap and serve that on the index page as a representation of
    the covid hotspots on campus.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
 from flask import render_template
+from folium import Map
+from folium.plugins import HeatMap
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html")
+	start_coords = (30.61722222222222, -96.3338888888889)
+	hmap = Map(location=start_coords, zoom_start=15)
+	hmap_element = HeatMap([(30.615250, -96.335952)],
+			min_opacity=1, max_val=5, radius=17, blur=15)
+	hmap.add_child(hmap_element)
+	return render_template('index.html', hmap=hmap)
+
